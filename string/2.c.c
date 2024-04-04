2dd3a7d69cce6f5ca97a1d720230dcc997edbe5c
amitdervadiya <amitdervadiya@gmail.com>
2024-04-04 12:07:10 +0530

    string

---
+++ string/2.c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char a[]="HELLO WORLD";
+    for (int i = 0; i < a[i] !=NULL; i++)
+    {
+        if (a[i]>='A'&& a[i]<='Z')
+        {
+            a[i]+=32;
+        }
+            printf("%c",a[i]);
+
+    }
+    return 0;
+}
